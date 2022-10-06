ace5df3a67b9d45258552668646bb89824365cb1
meteaki <94170624+meteaki@users.noreply.github.com>
2022-10-06 22:30:20 +0200

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -5,6 +5,7 @@ from PIL import Image
 import sklearn as sklearn
 import pickle
 from sklearn.preprocessing import OneHotEncoder
+import IPython
 
 st.set_page_config(layout="wide")
 image = Image.open("image.png")
